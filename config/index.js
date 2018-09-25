f433b364d03fa43cd10b32eb5f125024a3a00993
Adam <adam.aak@gmail.com>
2018-09-25 15:14:43 -0400

    Adding some junk

---
+++ config/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Save this file as `index.js` in this folder with your account's IDs.
+// `index.js` is .gitignored so it won't accidentally end up somewhere
+// it shouldn't.
+
+module.exports = exports = {
+  clientId: 'ca_BuLSGj1JOwyfHoqTmVvnypg8ykERAlSz', // Your client ID: https://dashboard.stripe.com/account/applications/settings
+  secretKey: 'sk_test_r5djZLubk2F9JBONkPSP6fMc', // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
+  redirectUri: 'http://localhost:3000/connected', // https://dashboard.stripe.com/account/applications/settings
+
+  port: 3000
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var CONFIG = require('./config')
+
+var app = require('./lib/server');
+var stripe = require('stripe')(CONFIG.secretKey);
+var OAuth = require('client-oauth2');
+
+// Configure the OAuth 2.0 Client
+var oauth = new OAuth({
+    clientId: CONFIG.clientId,
+    clientSecret: CONFIG.secretKey,
+
+    scopes: ['read_write'],
+
+    redirectUri: CONFIG.redirectUri,
+    authorizationUri: 'https://connect.stripe.com/oauth/authorize',
+    accessTokenUri: 'https://connect.stripe.com/oauth/token'
+});
+
+// Get the home page:
+app.get('/', function(req, res) {
+    res.render('index', {
+        redirectUri: oauth.code.getUri()
+    });
+});
+
+// Handle the redirect - this should match your CONFIG redirect's path:
+app.get('/connected', function(req, res) {
+    if (req.query.error) return response.render('connected', {
+        error: req.query.error
+    });
+
+    // Use the Authorization Code to get a Token
+    oauth.code.getToken(req.url).then(handleToken);
+
+    // Go fetch the Account from the Token
+    function handleToken(token) {
+        stripe.account.retrieve(token.data.stripe_user_id, onAccount);
+    }
+
+    // Render the Account information
+    function onAccount(error, account) {
+        res.render('connected', {
+            error: error,
+            account: account
+        });
+    }
+});
+
+app.listen(process.env.PORT || CONFIG.port)
